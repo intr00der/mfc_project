f19bee51c20a6cdc886222de6caf38c6910e8591
intr00der <69047651+intr00der@users.noreply.github.com>
2020-08-21 22:06:43 +0300

    Added button pages (#1)
    Improved code

--- forms_gui/views.py
+++ forms_gui/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-from .models import FormBody, FormField, FormButton
-from django.views.generic import CreateView, ListView, View
+import os
+
+from .models import FormButton
+from django.views.generic import ListView
 
 
 class HomeView(ListView):
+    queryset = FormButton.objects.filter(parent__isnull=True)
+    template_name = os.path.join('forms_gui', 'home.html')
+    context_object_name = 'buttons'
+
+
+class ButtonView(ListView):
+    queryset = FormButton.objects.all()
+    template_name = os.path.join('forms_gui', 'buttons.html')
+    context_object_name = 'buttons'
 
     def get_queryset(self):
-        queryset = {
-            FormButton.objects.all()
-        }
-        return render('form_fields.html', {'queryset': queryset})
+        return super().get_queryset().filter(
+            parent_id=self.kwargs['id']
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['button_title'] = FormButton.objects.get(pk=self.kwargs['id'])
+        return context
+
+
+class FormBodyView(ListView):
+    pass
