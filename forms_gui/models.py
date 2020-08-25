f9b6f6a9c170ed9dcfa8a0ae01ef72add07b5d9d
intr00der <69047651+intr00der@users.noreply.github.com>
2020-08-25 20:33:40 +0300

    Make form fields and begin template input objects (#2)

--- forms_gui/models.py
+++ forms_gui/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.urls import reverse_lazy
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 class FormField(models.Model):
     FORM_TYPE_CHOICES = [('text', 'Текст'),
-                         ('radio', 'Радио кнопка'),
+                         ('radio', 'Радио кнопки'),
                          ('dropdown', 'Выпадающий список'),
                          ('date', 'Дата'),
                          ('checkbox', 'Чекбокс')]
@@ -19,6 +20,9 @@ class FormField(models.Model):
         verbose_name = 'Поле формы'
         verbose_name_plural = 'Поля форм'
 
+    def data_as_list(self):
+        return str(self.data).split('\n')
+
     def __str__(self):
         return self.title
 
@@ -26,7 +30,7 @@ class FormField(models.Model):
 class FormBody(models.Model):
     title = models.CharField('Заголовок', max_length=255)
     details = models.CharField(max_length=255, blank=True, null=True)
-    form_fields = models.ManyToManyField(FormField)
+    form_fields = models.ManyToManyField(FormField, related_name='form_body')
 
     class Meta:
         default_related_name = 'form_bodies'
@@ -38,19 +42,19 @@ class FormBody(models.Model):
 
 
 class FormButton(MPTTModel):
-    title = models.CharField('Заголовок', max_length=255)
+    title = models.CharField('Заголовок', max_length=255, blank=True, null=True)
     parent = TreeForeignKey('self',
                             null=True,
                             blank=True,
                             related_name='children',
                             on_delete=models.CASCADE,
-
                             )
     form_body = models.OneToOneField(FormBody,
                                      null=True,
                                      blank=True,
-                                     verbose_name="form_body",
-                                     on_delete=models.CASCADE
+                                     verbose_name='form_body',
+                                     on_delete=models.CASCADE,
+                                     related_name='form_button',
                                      )
 
     class Meta:
--- forms_gui/views.py
+++ forms_gui/views.py
@@ -1,30 +1,57 @@
 import os
 
-from .models import FormButton
+from .models import FormButton, FormBody, FormField
 from django.views.generic import ListView
 
 
-class HomeView(ListView):
-    queryset = FormButton.objects.filter(parent__isnull=True)
-    template_name = os.path.join('forms_gui', 'home.html')
-    context_object_name = 'buttons'
-
-
-class ButtonView(ListView):
+class ButtonMixin:
     queryset = FormButton.objects.all()
     template_name = os.path.join('forms_gui', 'buttons.html')
     context_object_name = 'buttons'
+    is_start_page = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['button_title'] = ('МНОГОФУНКЦИОНАЛЬНЫЙ ЦЕНТР' if self.is_start_page else
+                                   FormButton.objects.get(pk=self.kwargs['id']))
+        return context
+
+
+class HomeView(ButtonMixin, ListView):
+    queryset = FormButton.objects.filter(parent__isnull=True)
+
+
+class ButtonView(ButtonMixin, ListView):
+    is_start_page = False
 
     def get_queryset(self):
         return super().get_queryset().filter(
             parent_id=self.kwargs['id']
         )
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(object_list=object_list, **kwargs)
-        context['button_title'] = FormButton.objects.get(pk=self.kwargs['id'])
-        return context
+
+class FormBodyView(ListView):
+    template_name = os.path.join('forms_gui', 'form_fields.html')
+    context_object_name = 'form_bodies'
+    queryset = FormBody.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            id=self.kwargs['form_id']
+        )
 
 
 class FormBodyView(ListView):
-    pass
+    template_name = os.path.join('forms_gui', 'form_fields.html')
+    context_object_name = 'form_fields'
+    queryset = FormField.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            form_body__id=self.kwargs['form_id']
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form_body_title'] = FormBody.objects.get(pk=self.kwargs['form_id'])
+        return context
